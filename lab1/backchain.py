5a26f77529b0433e6115220e700da18d3f7101e6
DanielBBHub <63246222+DanielBBHub@users.noreply.github.com>
2025-11-12 17:27:48 +0100

    Backward chaining resuelto

--- lab1/backchain.py
+++ lab1/backchain.py
@@ -1,5 +1,6 @@
 from production import AND, OR, NOT, PASS, FAIL, IF, THEN, \
      match, populate, simplify, variables
+
 from zookeeper import ZOOKEEPER_RULES
 
 # This function, which you need to write, takes in a hypothesis
@@ -15,8 +16,52 @@ from zookeeper import ZOOKEEPER_RULES
 
 
 def backchain_to_goal_tree(rules, hypothesis):
-    raise NotImplementedError
+    # Paso 1: Buscar reglas que puedan probar la hipótesis
+    matching_rules = []
+    alternatives = []
+    for rule in rules:
+        # Verificar si la conclusión de la regla coincide con la hipótesis
+        bindings = match(rule.consequent()[0], hypothesis)
+        if bindings is not None:
+            matching_rules.append((rule, bindings))
+    # print(f"MATCHING RULES? {not matching_rules}")
+    # Paso 2: Si no hay reglas que prueben la hipótesis, es un hecho base
+    if not matching_rules:
+        # print(f"ALTERNATIVAS: {alternatives}")
+        return hypothesis    
+    
+    
+    add_rules = [populate(rule[0].antecedent(), rule[1])  for rule in matching_rules]
+    for rule_antecedents in add_rules:
+        # print(f"REGLA ANTECEDENTES: {rule_antecedents} DE TIPO: {type(rule_antecedents)}")
+        if isinstance(rule_antecedents, str):
+            subtree = backchain_to_goal_tree(rules, rule_antecedents)
+            alternatives.append(subtree)
+        elif len(rule_antecedents) == 1:
+            # Solo una condición, hacer backchain directamente
+            
+            subtree = backchain_to_goal_tree(rules, rule_antecedents[0])
+            alternatives.append(subtree)
+        else:
+            # Múltiples condiciones, crear un nodo AND
+            and_branches = []
+            for hyp_ex in rule_antecedents:
+                subtree = backchain_to_goal_tree(rules, hyp_ex)
+                and_branches.append(subtree)
+            alternatives.append(AND(and_branches))
+
+            
+    
+    flattened_alternatives = []
+    for alt in alternatives:
+        if isinstance(alt, OR):
+            # Si la alternativa es un OR, agregar sus elementos directamente
+            flattened_alternatives.extend(alt)
+        else:
+            flattened_alternatives.append(alt)
+    return OR([hypothesis] + flattened_alternatives)
+
 
 # Here's an example of running the backward chainer - uncomment
 # it to see it work:
-#print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
+print( backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin'))
